1265f8de5825c50193a6dd392fa43cb268457a64
Dan Dowd <danieldowd89@gmail.com>
2023-04-26 11:41:44 -0600

    changes output names

--- src/infrastructure/lib/user-stack.ts
+++ src/infrastructure/lib/user-stack.ts
@@ -1,17 +1,45 @@
 import * as cdk from "aws-cdk-lib";
+import { SecretValue } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class UserStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const userPool = new cdk.aws_cognito.UserPool(this, "UserPool", {});
+
+    new cdk.aws_cognito.UserPoolIdentityProviderGoogle(
+      this,
+      "GoogleIdProvider",
+      {
+        userPool,
+        clientId:
+          "69790275993-vvpfap89obu9pno4soikkngr24q52gs6.apps.googleusercontent.com",
+        clientSecretValue: SecretValue.secretsManager(
+          "arn:aws:secretsmanager:us-east-1:589332345969:secret:staging/workout/google/idProvider-SXUXnE"
+        ),
+      }
+    );
+
+    const authorizer = new cdk.aws_apigateway.CognitoUserPoolsAuthorizer(
+      this,
+      "Authorizer",
+      {
+        cognitoUserPools: [userPool],
+      }
+    );
+
+    new cdk.CfnOutput(this, "UserAuthorizerId", {
+      value: authorizer.authorizerId,
+    });
+
     const api = new cdk.aws_apigateway.RestApi(this, "UserApi", {});
 
-    new cdk.CfnOutput(this, "ApiId", {
+    new cdk.CfnOutput(this, "UserApiId", {
       value: api.restApiId,
     });
 
-    new cdk.CfnOutput(this, "ApiRootResourceId", {
+    new cdk.CfnOutput(this, "UserApiRootResourceId", {
       value: api.restApiRootResourceId,
     });
 
@@ -29,7 +57,10 @@ export class UserStack extends cdk.Stack {
 
     api.root.addMethod(
       "POST",
-      new cdk.aws_apigateway.LambdaIntegration(startFunction)
+      new cdk.aws_apigateway.LambdaIntegration(startFunction),
+      {
+        authorizer,
+      }
     );
   }
 }
