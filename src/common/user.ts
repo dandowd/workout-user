b9750ae6f227cd597c807c2507f72db076d907bd
Dan Dowd <danieldowd89@gmail.com>
2023-05-12 15:55:18 -0600

    gets user id from authorizer

--- src/common/user.ts
+++ src/common/user.ts
@@ -1,7 +1,8 @@
-import { Context } from "aws-lambda";
+import { APIGatewayProxyEvent } from "aws-lambda";
 
-export const getUserId = (context: Context) => {
-  const userId = context.identity?.cognitoIdentityId;
+export const getUserId = (event: APIGatewayProxyEvent) => {
+  const userId = event.requestContext.authorizer?.claims.sub;
+
   if (!userId) {
     throw new Error("User is not authenticated");
   }
