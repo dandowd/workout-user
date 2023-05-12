bee2b1729adb81427e5afd6f9b4a31d22012d560
Dan Dowd <danieldowd89@gmail.com>
2023-05-12 09:25:20 -0600

    adds log to start

--- src/user-info/start-handler.ts
+++ src/user-info/start-handler.ts
@@ -1,5 +1,11 @@
+import { logger } from "../common/logger";
+
 const start = async (event: any) => {
-  console.log("start");
+  logger.info("start", { event });
+
+  return {
+    statusCode: 200,
+  };
 };
 
 export const handler = start;
